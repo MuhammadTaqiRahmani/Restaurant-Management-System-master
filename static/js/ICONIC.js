bc43f325c20efee85eb4e7c41f2e6a054aca9f03
Muhammad Taqi Rahmani <taqirehmani111@gmail.com>
2024-06-20 00:20:09 +0500

    Updation Algorithm for Orders and items + Improvement of searching algorithm

---
+++ static/js/ICONIC.js
@@ -0,0 +1,20 @@
+function searchTable() {
+    var input, filter, table, tr;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.querySelector(".table");
+    tr = table.getElementsByTagName("tr");
+  
+    for (let i = 1; i < tr.length; i++) {
+      let txtValue = Array.from(tr[i].getElementsByTagName("td"))
+                          .map(td => td.textContent || td.innerText)
+                          .join(' ');
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        tr[i].classList.add("highlight");
+      } else {
+        tr[i].style.display = "none";
+        tr[i].classList.remove("highlight");
+      }
+    }
+  }
